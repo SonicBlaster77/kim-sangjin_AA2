2658549efebefab1044ec246e88912ebca62056b
SonicBlaster77 <125948492+SonicBlaster77@users.noreply.github.com>
2023-04-10 23:18:52 +0800

    Create while.js

---
+++ while.js
@@ -0,0 +1,13 @@
+var x = 1
+
+while(x < 52){
+    if(x<51){
+        document.write(x+ ", ");
+        x++;
+        x++;
+    }else{
+        document.write(x);
+        x++;
+        x++;
+    }
+}
